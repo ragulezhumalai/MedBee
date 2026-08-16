7040a83d7723c9437ed9878641b94870c8528adf
Ragulezhumalai <raghulelumalai@gmail.com>
2026-08-16 22:33:28 +0530

    UI fixes 0.3.2

--- src/components/toolWrapper.jsx
+++ src/components/toolWrapper.jsx
@@ -1,6 +1,9 @@
 import {React,useState} from 'react'
 import Mue from './Mue'
 import {LiquidGlass} from '@liquidglass/react'
+import mue from '../assets/mue.png'
+import auth from '../assets/auth.png'
+import clia from '../assets/Clia.png'
 
 const ToolWrapper = ({nowTools}) => {
 
@@ -19,9 +22,9 @@ const changeTool=set=>{
         displacementScale={2}
         className="Tint">
       <ul className='grid z-0 grid-cols-2 text-sm sm:text-lg  sm:grid-cols-4 gap-2 justify-evenly mueOutput md:grid-cols-auto gap-auto p-4 m-5 text-left h-[60dvh] sm:max-h-[60dvh] sm:max-w-[130%] item-center'>
-        <button className='  Menu  ' onClick={()=>changeTool("Mue")} > <img src="/src/assets/mue.png" alt="MUE" className="sm:w-16 m-2 sm:h-16 mx-auto" /> <div className=' text-center'>MUE Edits</div> </button>
-        <button className=' Menu ' onClick={()=>changeTool("Auth")} > <img src="/src/assets/auth.png" alt="Auth" className="sm:w-16 sm:h-16 m-2 mx-auto" /> <div className=' text-center'>Auth lookup</div> </button>
-        <button className=' Menu ' onClick={()=>changeTool("alltools")} > <img src="/src/assets/Clia.png" alt="Findcare" className="sm:w-16 m-2 sm:h-16 mx-auto" /> <div className=' text-center'>(coming soon)</div> </button>
+        <button className='  Menu  ' onClick={()=>changeTool("Mue")} > <img src={mue} alt="MUE" className="sm:w-16 m-2 sm:h-16 mx-auto" /> <div className=' text-center'>MUE Edits</div> </button>
+        <button className=' Menu ' onClick={()=>changeTool("Auth")} > <img src={auth} alt="Auth" className="sm:w-16 sm:h-16 m-2 mx-auto" /> <div className=' text-center'>Auth lookup</div> </button>
+        <button className=' Menu ' onClick={()=>changeTool("alltools")} > <img src={clia} alt="Findcare" className="sm:w-16 m-2 sm:h-16 mx-auto" /> <div className=' text-center'>(coming soon)</div> </button>
       </ul>
 
 
--- src/components/tools/auth.jsx
+++ src/components/tools/auth.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import { LiquidGlass } from '@liquidglass/react';
+import humana from '../../assets/authlogo/Humana.png'
+import cgs from '../../assets/authlogo/cgs.png'
+import aetna from '../../assets/authlogo/Aetna.png'
+import anthem from '../../assets/authlogo/Anthem.png'
+import alliance from '../../assets/authlogo/Alliance.png'
+import wellcare from '../../assets/authlogo/Wellcare.png'
+import molina from '../../assets/authlogo/Molina.png'
 
 const auth = () => {
   return (
@@ -14,14 +21,14 @@ const auth = () => {
         className='Tint mueOutput    min-h-[50%] h-[50%] rounded-lg flex flex-col  outline-2 outline-white/30  md:w-[60%]'>
           Auth Lookup Tools
       <ul className="grid grid-cols-2  md:grid-cols-5 gap-5 p-4 text-left overflow-hidden w-[95%] h-[90%] item-center"> 
-        <li className=" Menu authtab text-center"><a href="https://www.cgsmedicare.com/medicare_dynamic/jc/pa/pa.aspx" target="_blank"> <img src="/src/assets/authlogo/cgs.png" alt="CGS Medicare" className="sm:w-16 sm:h-16 m-2 mx-auto" /> CGS Medicare</a></li>
-        <li className=" Menu authtab  text-center"><a href="https://www.aetna.com/health-care-professionals/precertification/precertification-lists.html" target="_blank">  <img src="/src/assets/authlogo/Aetna.png" alt="CGS Medicare" className="sm:w-16 sm:h-16 m-2 mx-auto" /> Aetna</a></li>
-        <li className=" Menu authtab  text-center"><a href="https://provider.healthybluemo.com/missouri-provider/resources/precertification-lookup" target="_blank"> <img src="/src/assets/authlogo/Anthem.png" alt="CGS Medicare" className="sm:w-16 sm:h-16 m-2 mx-auto" /> Anthem Missouri</a></li>
-        <li className=" Menu authtab  text-center"><a href="https://providers.anthem.com/wisconsin-provider/claims/prior-authorization-lookup-tool" target="_blank"> <img src="/src/assets/authlogo/Anthem.png" alt="CGS Medicare" className="sm:w-16 sm:h-16 m-2 mx-auto" /> Anthem Wisconsin</a></li>
-        <li className=" Menu authtab  text-center"><a href="https://provider.humana.com/coverage-claims/prior-authorizations/prior-authorizations-search-tool" target="_blank">  <img src="/src/assets/authlogo/Humana.png" alt="CGS Medicare" className="sm:w-16 sm:h-16 m-2 mx-auto" />Humana</a></li>
-        <li className=" Menu authtab  text-center"><a href="https://www.alliancehealthplan.org/providers/procedure-code-lookup-tool/" target="_blank"> <img src="/src/assets/authlogo/Alliance.png" alt="CGS Medicare" className="sm:w-16 sm:h-16 m-2 mx-auto" />Alliance Health</a></li>
-         <li className=" Menu authtab  text-center"><a href="https://www.wellcare.com/en/authorization-lookup" target="_blank"> <img src="/src/assets/authlogo/Wellcare.png" alt="CGS Medicare" className="sm:w-16 sm:h-16 m-2 mx-auto" />Wellcare</a></li>
-          <li className=" Menu authtab  text-center"><a href="https://www.molinahealthcare.com/providers/ca/medicaid/palookup" target="_blank"> <img src="/src/assets/authlogo/Molina.png" alt="CGS Medicare" className="sm:w-16 sm:h-16 m-2 mx-auto" />Molina Healthcare</a></li>
+        <li className=" Menu authtab text-center"><a href="https://www.cgsmedicare.com/medicare_dynamic/jc/pa/pa.aspx" target="_blank"> <img src={cgs} alt="CGS Medicare" className="sm:w-16 sm:h-16 m-2 mx-auto" /> CGS Medicare</a></li>
+        <li className=" Menu authtab  text-center"><a href="https://www.aetna.com/health-care-professionals/precertification/precertification-lists.html" target="_blank">  <img src={aetna} alt="CGS Medicare" className="sm:w-16 sm:h-16 m-2 mx-auto" /> Aetna</a></li>
+        <li className=" Menu authtab  text-center"><a href="https://provider.healthybluemo.com/missouri-provider/resources/precertification-lookup" target="_blank"> <img src={anthem} alt="CGS Medicare" className="sm:w-16 sm:h-16 m-2 mx-auto" /> Anthem Missouri</a></li>
+        <li className=" Menu authtab  text-center"><a href="https://providers.anthem.com/wisconsin-provider/claims/prior-authorization-lookup-tool" target="_blank"> <img src={anthem} alt="CGS Medicare" className="sm:w-16 sm:h-16 m-2 mx-auto" /> Anthem Wisconsin</a></li>
+        <li className=" Menu authtab  text-center"><a href="https://provider.humana.com/coverage-claims/prior-authorizations/prior-authorizations-search-tool" target="_blank">  <img src={humana} alt="CGS Medicare" className="sm:w-16 sm:h-16 m-2 mx-auto" />Humana</a></li>
+        <li className=" Menu authtab  text-center"><a href="https://www.alliancehealthplan.org/providers/procedure-code-lookup-tool/" target="_blank"> <img src={alliance} alt="CGS Medicare" className="sm:w-16 sm:h-16 m-2 mx-auto" />Alliance Health</a></li>
+         <li className=" Menu authtab  text-center"><a href="https://www.wellcare.com/en/authorization-lookup" target="_blank"> <img src={wellcare} alt="CGS Medicare" className="sm:w-16 sm:h-16 m-2 mx-auto" />Wellcare</a></li>
+          <li className=" Menu authtab  text-center"><a href="https://www.molinahealthcare.com/providers/ca/medicaid/palookup" target="_blank"> <img src={molina} alt="CGS Medicare" className="sm:w-16 sm:h-16 m-2 mx-auto" />Molina Healthcare</a></li>
    
 
 
